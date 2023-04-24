897b5803beabaf9ddf42f03368b6fe00d4fc868f
Aleebaba001 <aliyumuhammed523@gmail.com>
2023-04-24 11:09:08 +0100

    Added My First Java Project folder to the TestCodeCamp repository

---
+++ FirstJava_Poject/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        // Print the welcome message
+        System.out.printf("Hello and welcome!");
+
+        // Print the message
+        System.out.println("Hello world, this is my first Java project");
+        System.out.println("Software testing no easy but we will prevail");
+        System.out.println("I want to land a QA role that will pay me $15k a month just to sit at home and record test cases on google sheets. Amen x10.");
+        System.out.println("What came first? The chicken or the egg?");
+        System.out.println("Speaking of eggs, they are a good source of protein when you cannot afford meat in your soup. I'm speaking from experience. Thank me later.");
+
+
+
+        // End the program
+        System.exit(0);
+    }
+}
